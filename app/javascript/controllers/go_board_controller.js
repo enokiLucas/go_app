402793b7344c9f3c37e97a172e0498e6603fe86d
Lucas Enoki <enoki.lucas@gmail.com>
2024-08-07 16:44:24 -0300

    continue testing

--- app/javascript/controllers/go_board_controller.js
+++ app/javascript/controllers/go_board_controller.js
@@ -5,13 +5,15 @@ export default class extends Controller {
   connect() {
     this.element.innerHTML = `<go-board></go-board>`;
     this.testEventListeners();
-    console.log('hello111');
+    console.log('hello from connect');
   }
 
   testEventListeners() {
-    console.log('ciaoqq')
+    console.log('hello from test')
     document.addEventListener('testing-event', () => {
-      console.log('helloABC');
+      console.log('hello after event');
     });
   }
+
+  
 }
