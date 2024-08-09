aacad32452c999fd1a98cb982ad927610fff2ef8
Lucas Enoki <enoki.lucas@gmail.com>
2024-08-09 14:12:52 -0300

    End tests

--- app/javascript/controllers/go_board_controller.js
+++ app/javascript/controllers/go_board_controller.js
@@ -4,30 +4,6 @@ import '../components/GoBoard.js';
 export default class extends Controller {
   connect() {
     this.element.innerHTML = `<go-board></go-board>`;
-    this.testEventListeners();
-    this.testClick();
-    this.testEventFromGameStateManager();
     console.log('hello from connect');
-  }
-
-  testEventListeners() {
-    console.log('hello from test')
-    document.addEventListener('testing-event', () => {
-      console.log('hello after event');
-    });
-  }
-
-  testClick() {
-    console.log('hello from test click');
-    document.addEventListener('click', () => {
-      console.log('Listened to the click');
-    })
-  }
-
-  testEventFromGameStateManager() {
-    console.log('hello from test of GMS');
-    document.addEventListener('captures-changed', (e) => {
-      console.log(e.detail);
-    })
   }
 }
