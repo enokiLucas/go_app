441a2c2effae4ff2f789d6d7e37cca44b4bb7d11
Lucas Enoki <enoki.lucas@gmail.com>
2024-08-16 10:02:06 -0300

    Added shoelace buttons over board size buttons

--- app/javascript/controllers/button_board_size_controller.js
+++ app/javascript/controllers/button_board_size_controller.js
@@ -4,8 +4,13 @@ import '../components/PanelRight/buttons/ButtonBoardSize.js'
 export default class extends Controller {
   connect() {
     this.element.innerHTML = `
-    <button-board-size board-size="5"></button-board-size>
-    <button-board-size board-size="9"></button-board-size>
+    <sl-button>
+      <button-board-size board-size="5"></button-board-size>
+    </sl-button>
+
+    <sl-button>
+      <button-board-size board-size="9"></button-board-size>
+    </sl-button>
     `;
   }
 }
