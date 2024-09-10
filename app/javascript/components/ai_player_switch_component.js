fefb985e2d2f92418faaede6d9390c580ee84a05
Lucas Enoki <enoki.lucas@gmail.com>
2024-09-10 20:25:56 -0300

    Fix the switch not showing up by adding it to the index

--- app/javascript/components/ai_player_switch_component.js
+++ app/javascript/components/ai_player_switch_component.js
@@ -1,7 +1,10 @@
 class AiPlayerSwitch extends HTMLElement {
   constructor() {
-      super();
-      this.attachShadow({ mode: 'open'});
+    super();
+    this.attachShadow({ mode: 'open'});
+    this.shadowRoot.innerHTML = `
+      <sl-switch checked>Switch</sl-switch>
+    `;
   }
 }
 
--- app/javascript/controllers/ai_player_switch_controller.js
+++ app/javascript/controllers/ai_player_switch_controller.js
@@ -2,11 +2,9 @@ import { Controller } from '@hotwired/stimulus';
 import '../components/ai_player_switch_component.js';
 
 export default class extends Controller {
-    connect() {
-        this.element.innerHTML = `
-            <form class="switch-ai-player">
-                <sl-switch>AI player</sl-switch>
-            </form>
-        `;
-    }
+  connect() {
+    this.element.innerHTML = `
+      <ai-player-switch></ai-player-switch>
+    `;
+  }
 }
