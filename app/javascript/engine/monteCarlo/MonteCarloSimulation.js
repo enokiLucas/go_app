8a5652e98198575dffa31dfaa531143924f5f15f
Lucas Enoki <enoki.lucas@gmail.com>
2024-11-20 15:53:11 -0300

    Formatting and fixes

--- app/javascript/engine/monteCarlo/MonteCarloSimulation.js
+++ app/javascript/engine/monteCarlo/MonteCarloSimulation.js
@@ -1,17 +1,16 @@
 // Logic for running the simulations
-import { territoryScoring } from "../../services/score/TerritoryScoring.js";
-import { gameStateManager } from "../../services/GameStateManager.js";
+import { territoryScoring } from '../../services/score/TerritoryScoring.js';
 
 class MonteCarloSimulation {
   static simulate(state) {
     let simulationState = state.clone();
     if (!MonteCarloSimulation.isTerminal(simulationState)) {
       const move = MonteCarloSimulation.getHeuristicMove(simulationState);
-      console.log(move);
+      //console.log(move);
       move.then((result) => {
         if (result === null) {
           //No valid moves. Pass move
-          console.log("move === null");
+          console.log('move === null');
           simulationState.passCounter += 1;
         } else {
           const [x, y] = result;
@@ -20,6 +19,7 @@ class MonteCarloSimulation {
         }
       });
     }
+
     return {
       move: `${simulationState.lastMoveX},${simulationState.lastMoveY}`,
       score: MonteCarloSimulation.score(simulationState),
@@ -39,17 +39,16 @@ class MonteCarloSimulation {
         }
       }
     }
-    //console.log(availableMoves[Math.floor(Math.random() * availableMoves.length)]);
     return availableMoves[Math.floor(Math.random() * availableMoves.length)];
   }
 
   static async getHeuristicMove(state) {
     return new Promise((resolve, reject) => {
-      const workerPath = new URL("heuristicWorker.js", import.meta.url);
+      const workerPath = new URL('heuristicWorker.js', import.meta.url);
       const worker = new Worker(workerPath);
-      //const worker = new Worker("./heuristicWorker.js");
+      //const worker = new Worker('./heuristicWorker.js');
       worker.postMessage(state);
-      worker.addEventListener("message", (e) => {
+      worker.addEventListener('message', (e) => {
         const selectedMove = e.data;
         resolve(selectedMove);
       });
--- app/javascript/services/score/TerritoryScoring.js
+++ app/javascript/services/score/TerritoryScoring.js
@@ -1,83 +1,85 @@
-import { rulesControl } from '../RulesControl.js';
 import { exploreTerritory } from '../../utils/ScoreUtil.js';
 
 class TerritoryScoring {
-	constructor() {
-		//this.scoringBoard = rulesControl.createSimulatedBoardMatrix(); // Assuming this creates a deep copy of the board
-		this.resetTerritoriesCount();
-		this.blackTerritory = 0;
-		this.whiteTerritory = 0;
-	}
+  constructor() {
+    //this.scoringBoard = rulesControl.createSimulatedBoardMatrix(); // Assuming this creates a deep copy of the board
+    this.resetTerritoriesCount();
+    this.blackTerritory = 0;
+    this.whiteTerritory = 0;
+  }
 
-	resetTerritoriesCount() {
-		this.blackTerritory = 0;
-		this.whiteTerritory = 0;
-		this.visited = new Set();
-	}
+  resetTerritoriesCount() {
+    this.blackTerritory = 0;
+    this.whiteTerritory = 0;
+    this.visited = new Set();
+  }
 
-	/**
-	 * In monte carlo simulation, countScore has am argument that is the state boardMatrix
-	 * but in the EndGameManager, it is used without an arg as it was first designed.
-	 * 
-	 * Make the function below receive an argument of the matrix
-	 * 
-	 * Try the following first:
-	 * countScore(const scoringBoard = rulesControl.createSimulatedBoardMAtrix)
-	*/
+  /**
+   * In monte carlo simulation, countScore has am argument that is the state boardMatrix
+   * but in the EndGameManager, it is used without an arg as it was first designed.
+   *
+   * Make the function below receive an argument of the matrix
+   *
+   * Try the following first:
+   * countScore(const scoringBoard = rulesControl.createSimulatedBoardMAtrix)
+   */
 
-	addTerritoryToScore() {
-		// Add territory size to the corresponding player's score
-		if (surroundedBy === 'black') {
-			this.blackTerritory += points.length;
-		} else if (surroundedBy === 'white') {
-			this.whiteTerritory += points.length;
-		}
-	}
+  addTerritoryToScore() {
+    // Add territory size to the corresponding player's score
+    if (surroundedBy === 'black') {
+      this.blackTerritory += points.length;
+    } else if (surroundedBy === 'white') {
+      this.whiteTerritory += points.length;
+    }
+  }
 
-	/**
-	 * 
-	 * @param {Array} scoringBoard matrix made based on the board that can be manipulated without interfering with the original
-	 * @param {boolean} bool switch for using the function addTerritoryToScore
-	 * 	true: add the territory to the score
-	 * 	false: does not add territory to the score. 
-	 */
-	countScore(scoringBoard, bool) {
-		for (let x = 0; x < scoringBoard.length; x++) {
-			for (let y = 0; y < scoringBoard[x].length; y++) {
-				if (scoringBoard[x][y] === null && !this.visited.has(`${x},${y}`)) {
-					const { points, isCompletelySurrounded, surroundedBy } = exploreTerritory(scoringBoard, x, y);
-					if (isCompletelySurrounded) {
-						// Mark all explored territory points as visited
-						points.forEach(point => {
-							this.visited.add(`${point.x},${point.y}`);
-						});
-						// In case countScore is used without the intention of calculating the final score.
-						if (bool) {
-							this.addTerritoryToScore;
-						} else {
-							return points.length;
-						}
-					}
-				}
-			}
-		}
+  /**
+   *
+   * @param {Array} scoringBoard matrix made based on the board that can be manipulated without interfering with the original
+   * @param {boolean} bool switch for using the function addTerritoryToScore
+   * 	true: add the territory to the score
+   * 	false: does not add territory to the score.
+   */
+  countScore(scoringBoard, bool) {
+    for (let x = 0; x < scoringBoard.length; x++) {
+      for (let y = 0; y < scoringBoard[x].length; y++) {
+        if (scoringBoard[x][y] === null && !this.visited.has(`${x},${y}`)) {
+          const { points, isCompletelySurrounded, surroundedBy } = exploreTerritory(scoringBoard, x, y);
+          console.log(
+            `points: ${points}; isCompletelySurrounded: ${isCompletelySurrounded}; surroundedBy: ${surroundedBy}`
+          );
+          if (isCompletelySurrounded) {
+            // Mark all explored territory points as visited
+            points.forEach((point) => {
+              this.visited.add(`${point.x},${point.y}`);
+            });
+            // In case countScore is used without the intention of calculating the final score.
+            if (bool) {
+              this.addTerritoryToScore;
+            } else {
+              return points.length;
+            }
+          } else {
+          }
+        }
+      }
+    }
+    const scoreTerritory = new CustomEvent('scoreTerritory', {
+      detail: {
+        blackTerritory: this.blackTerritory,
+        whiteTerritory: this.whiteTerritory,
+      },
+    });
+    document.dispatchEvent(scoreTerritory);
+  }
 
-		const scoreTerritory = new CustomEvent('scoreTerritory', {
-			detail: {
-				blackTerritory: this.blackTerritory,
-				whiteTerritory: this.whiteTerritory
-			}
-		});
-		document.dispatchEvent(scoreTerritory);
-	}
-
-	// Optional: Method to get current scores
-	getScores() {
-		return {
-			black: this.blackTerritory,
-			white: this.whiteTerritory
-		};
-	}
+  // Optional: Method to get current scores
+  getScores() {
+    return {
+      black: this.blackTerritory,
+      white: this.whiteTerritory,
+    };
+  }
 }
 
 export const territoryScoring = new TerritoryScoring();
