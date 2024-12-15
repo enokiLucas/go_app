84de19430d1a56a9a9677a64486f0814fc639de4
Lucas Enoki <enoki.lucas@gmail.com>
2024-12-15 16:48:10 -0300

    finish to implement isTerminal in the MCTS engine

--- app/javascript/engine/mcts/MCTSstate.js
+++ app/javascript/engine/mcts/MCTSstate.js
@@ -1,22 +1,33 @@
 // Handles state representation and cloning
 import { rulesControl } from "../../services/RulesControl";
+import { gameStateManager } from "../../services/GameStateManager";
 
 class mctsState {
-  constructor(boardMatrix, currentPlayer, passCounter, boardX, boardY) {
+  constructor(boardMatrix, currentPlayer, passCounter, boardX, boardY, movesMade) {
 
     if (!Array.isArray(boardMatrix) || boardMatrix.length === 0) {
       throw new Error("Invalid boardMatrix: must be a non-empty 2D array.");
     }
+
+    this.initializeEventListeners();
 
     this.boardMatrix = boardMatrix.map(row => [...row]);
     this.currentPlayer = currentPlayer;
     this.passCounter = passCounter;
     this.lastMoveX = boardX;
     this.lastMoveY = boardY;
+    this.movesMade = movesMade;
+    this.isTerminal = false;
   }
 
+  initializeEventListeners() {
+		document.addEventListener('end-game', () => {
+      this.isTerminal = true;
+		});
+	}
+
   clone() {
-    const clone = new MonteCarloState(this.boardMatrix, this.currentPlayer);
+    const clone = new mctsState(this.boardMatrix, this.currentPlayer);
     clone.passCounter = this.passCounter;
     clone.lastMoveX = this.lastMoveX;
     clone.lastMoveY = this.lastMoveY;
@@ -35,6 +46,26 @@ class mctsState {
       this.passCounter++;
     }
     return isValid.isValid;
+  }
+
+  arraysEqual(arr1, arr2) {
+    return arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
+  }
+
+  getAvailableMoves() {
+    const allMoves = [];
+    for (let i = 0; i < gameStateManager.boardSize; i++) {
+      for (let j = 0; j < gameStateManager.boardSize; j++) {
+        allMoves.push([i, j]);
+      }
+    }
+
+    const movesAvailable = allMoves.filter(
+      elem1 => !this.movesMade.some(
+        elem2 => this.arraysEqual(elem1, elem2)
+      )
+    );
+    return movesAvailable;
   }
 }
 
