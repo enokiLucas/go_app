473515987f674f08ee728bdd648f3eef663b4858
Lucas Enoki <enoki.lucas@gmail.com>
2025-01-06 16:54:54 -0300

    Fix worker loading

--- app/javascript/engine/mcts/MCTSworker.js
+++ app/javascript/engine/mcts/MCTSworker.js
@@ -1,9 +1,26 @@
-import { MCTS } from './MCTS.js'; // Adjust the import path as necessary
-import { mctsState } from './MCTSstate.js'; // Adjust the import path
+import { MCTS } from "./MCTS.js"; // Adjust the import path as necessary
+import { mctsState } from "./MCTSstate.js"; // Adjust the import path
+
+console.log("hello from worker 1");
 
 onmessage = (event) => {
+  try {
     const { gameState, iterations } = event.data;
-    const mcts = new MCTS(gameState, iterations);
+
+    // Ensure the gameState is properly serialized.
+    const state = new mctsState(
+      gameState.boardMatrix,
+      gameState.currentPlayer,
+      gameState.passCounter,
+      gameState.lastMoveX,
+      gameState.lastMoveY,
+      gameState.movesMade,
+    );
+
+    const mcts = new MCTS(state, iterations);
     const bestMove = mcts.run(); // Run the MCTS algorithm
     postMessage(bestMove); // Send the best move back to the main thread
+  } catch (error) {
+    postMessage({ error: error.message }); // Send the error message back to the main thread
+  }
 };
