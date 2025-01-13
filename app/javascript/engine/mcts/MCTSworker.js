e2b4eca2e4464645e97727be6e7f1f4b3cc05fce
Lucas Enoki <enoki.lucas@gmail.com>
2025-01-13 16:38:07 -0300

    Remove state from the worker

--- app/javascript/engine/mcts/MCTSworker.js
+++ app/javascript/engine/mcts/MCTSworker.js
@@ -1,26 +1,18 @@
-import { MCTS } from "./MCTS.js"; // Adjust the import path as necessary
-import { mctsState } from "./MCTSstate.js"; // Adjust the import path
-
-console.log("hello from worker 1");
+import { MCTS } from "./MCTS.js";
+import { mctsState } from "./MCTSstate.js";
 
 onmessage = (event) => {
+  console.log('hello from message', event.data);
   try {
     const { gameState, iterations } = event.data;
-
-    // Ensure the gameState is properly serialized.
-    const state = new mctsState(
-      gameState.boardMatrix,
-      gameState.currentPlayer,
-      gameState.passCounter,
-      gameState.lastMoveX,
-      gameState.lastMoveY,
-      gameState.movesMade,
-    );
-
-    const mcts = new MCTS(state, iterations);
+    console.log('stop 1');
+    const mcts = new MCTS(gameState, iterations);
+    console.log('stop 2');
     const bestMove = mcts.run(); // Run the MCTS algorithm
+    console.log('bestMove from the worker: ', bestMove);
     postMessage(bestMove); // Send the best move back to the main thread
   } catch (error) {
+    console.log('hello from error');
     postMessage({ error: error.message }); // Send the error message back to the main thread
   }
 };
