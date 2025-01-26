9b4772a239ff60e0b449c17742ffee640db8dca3
Lucas Enoki <enoki.lucas@gmail.com>
2025-01-25 21:32:10 -0300

    increase the padding in the buttons for the size of the board

--- app/javascript/controllers/button_board_size_controller.js
+++ app/javascript/controllers/button_board_size_controller.js
@@ -6,22 +6,22 @@ export default class extends Controller {
     this.element.innerHTML = `
 
       <p> Board Size: </p>
-      <sl-button id="slButton-5">
+      <sl-button id="slButton-5" class="button-size">
         5
         <button-board-size board-size="5"></button-board-size>
       </sl-button>
 
-      <sl-button id="slButton-9">
+      <sl-button id="slButton-9" class="button-size">
         9
         <button-board-size board-size="9"></button-board-size>
       </sl-button>
 
-      <sl-button id="slButton-13">
+      <sl-button id="slButton-13" class="button-size">
         13
         <button-board-size board-size="13"></button-board-size>
       </sl-button>
 
-      <sl-button id="slButton-19">
+      <sl-button id="slButton-19" class="button-size">
         19
         <button-board-size board-size="19"></button-board-size>
       </sl-button>
