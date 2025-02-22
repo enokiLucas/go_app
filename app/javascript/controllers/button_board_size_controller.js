eb56697494d0faee055cda508d61bcfc807ac500
Lucas Enoki <enoki.lucas@gmail.com>
2025-02-22 15:30:17 -0300

    replace the regular button of the board-size for shoelace radio buttons

--- app/javascript/controllers/button_board_size_controller.js
+++ app/javascript/controllers/button_board_size_controller.js
@@ -2,29 +2,5 @@ import { Controller } from '@hotwired/stimulus';
 import '../components/PanelRight/buttons/ButtonBoardSize.js'
 
 export default class extends Controller {
-  connect() {
-    this.element.innerHTML = `
-
-      <p> Board Size: </p>
-      <sl-button id="slButton-5" class="button-size">
-        5
-        <button-board-size board-size="5"></button-board-size>
-      </sl-button>
-
-      <sl-button id="slButton-9" class="button-size">
-        9
-        <button-board-size board-size="9"></button-board-size>
-      </sl-button>
-
-      <sl-button id="slButton-13" class="button-size">
-        13
-        <button-board-size board-size="13"></button-board-size>
-      </sl-button>
-
-      <sl-button id="slButton-19" class="button-size">
-        19
-        <button-board-size board-size="19"></button-board-size>
-      </sl-button>
-    `;
-  }
+  connect() {}
 }
