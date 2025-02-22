e5e9d7522481bb0784e2e68fef0d78147b3e55a3
Lucas Enoki <enoki.lucas@gmail.com>
2025-02-22 16:17:13 -0300

    Replace the switch that controls the AI for radio buttons

--- app/javascript/components/ai_player_switch_component.js
+++ app/javascript/components/ai_player_switch_component.js
@@ -4,17 +4,10 @@ class AiPlayerSwitch extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open'});
-    this.shadowRoot.innerHTML = `
-      <form class="switch-ai-player">
-        <p style="margin: 0px;">AI player</p>
-        <sl-switch id="ai-switch" help-text="Toggle the AI player" checked>AI Player is ON</sl-switch>
-      </form>
-    `;
   }
 
   connectedCallback() {
     const form = this.shadowRoot.querySelector('.switch-ai-player');
-    const switchLabel = this.shadowRoot.getElementById('ai-switch');
 
     form.addEventListener('sl-change', () => {
       const matchSettings = newMatchManager.getNewMatchSettings();
@@ -22,10 +15,8 @@ class AiPlayerSwitch extends HTMLElement {
 
       if (aiBool) {
         aiBool = false;
-        switchLabel.innerText = 'AI Player is OFF';
       } else {
         aiBool = true;
-        switchLabel.innerText = 'AI Player is ON';
       }
       newMatchManager.updateNewMatchSettings('aiPlayer', aiBool);
     });
