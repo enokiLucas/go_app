0ca94f431274a52b8557bf05db617e66514b7b8e
Lucas Enoki <enoki.lucas@gmail.com>
2025-02-23 19:14:17 -0300

    improve styles

--- app/javascript/controllers/pass_button_controller.js
+++ app/javascript/controllers/pass_button_controller.js
@@ -4,8 +4,8 @@ import '../components/PassButton.js';
 export default class extends Controller {
   connect() {
     this.element.innerHTML = `
-      <sl-button variant="warning" id="button-pass" class="lieu-button" size="medium">
-        Pass
+      <sl-button variant="warning" id="button-pass" class="lieu-button" size="small" circle>
+        <sl-icon name="caret-right-square-fill" label="pass move"></sl-icon>
         <pass-button></pass-button>
       </sl-button>
     `
