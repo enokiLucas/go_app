9ec1f875b65756397e210bbd0930909a00d4bdf3
Lucas Enoki <enoki.lucas@gmail.com>
2025-03-09 09:02:00 -0300

    run the update taks

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module GoApp
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -38,8 +38,5 @@ module GoApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    # Enable Template Helper
-    config.assets.paths << Rails.root.join('app', 'views', 'templates')
   end
 end
