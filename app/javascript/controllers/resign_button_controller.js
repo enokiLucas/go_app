bd279b6778a38501b01a02069fe3d8355355a493
Lucas Enoki <enoki.lucas@gmail.com>
2025-03-30 09:32:06 -0300

    Move the HTML of resign-button from the controller to its view

--- app/javascript/controllers/resign_button_controller.js
+++ app/javascript/controllers/resign_button_controller.js
@@ -2,12 +2,5 @@ import { Controller } from '@hotwired/stimulus';
 import '../components/ResignButton.js';
 
 export default class extends Controller {
-  connect() {
-    this.element.innerHTML = `
-      <sl-button variant="danger" id="button-resign" class="lieu-button" size="small" circle>
-        <sl-icon name="flag-fill" label="Resign"></sl-icon>
-        <resign-button></resign-button>
-      </sl-button>
-    `
-  }
+  connect() {}
 }
