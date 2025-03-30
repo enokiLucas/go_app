e5abc5ab5d345fc5c801489d1ad4b119029db621
Lucas Enoki <enoki.lucas@gmail.com>
2025-03-30 09:31:06 -0300

    Move the HTML of pass-button from the controller to its view

--- app/javascript/controllers/pass_button_controller.js
+++ app/javascript/controllers/pass_button_controller.js
@@ -2,12 +2,5 @@ import { Controller } from '@hotwired/stimulus';
 import '../components/PassButton.js';
 
 export default class extends Controller {
-  connect() {
-    this.element.innerHTML = `
-      <sl-button variant="warning" id="button-pass" class="lieu-button" size="small" circle>
-        <sl-icon name="caret-right-square-fill" label="pass move"></sl-icon>
-        <pass-button></pass-button>
-      </sl-button>
-    `
-  }
+  connect() {}
 }
